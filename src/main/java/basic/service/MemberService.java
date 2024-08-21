feed9c3d2ef06594219664a372b5be3de57a2ef1
kang <dmschd873@gmail.com>
2024-08-21 17:36:29 +0900

    기본 구조 세팅 정정

--- src/main/java/basic/service/MemberService.java
+++ src/main/java/basic/service/MemberService.java
@@ -3,9 +3,11 @@ package basic.service;
 import basic.repository.MemberRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@Getter
+@Service
 @RequiredArgsConstructor
 public class MemberService {
+
     private final MemberRepository memberRepository;
 }
